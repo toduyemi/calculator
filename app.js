9f8ea7a0382cc7a059512b785597609487260eb5
toduyemi <temit96@hotmail.com>
2023-08-19 17:44:34 -0400

    complete display and operation functionalities
    Added eventlisteners for number buttons and operation buttons

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// operation functions
 function add(x, y) {
     return x + y;
 }
@@ -17,23 +18,77 @@ function divide(x, y) {
 function operate(sign, x, y) {
     switch (sign) {
         case ('+'):
-            return add(x, y);
+            return add(+x, +y);
             break;
         case ('-'):
-            return subtract(x, y);
+            return subtract(+x, +y);
             break;
         case ('*'):
             return multiply(args);
             break;
         case ('/'):
-            return divide(x, y);
+            return divide(+x, +y);
             break;
     }
 
 }
 
-let firstNumber;
-let secondNumber;
+let firstNumber = {
+    storedCheck: false,
+};
+let secondNumber = {
+    storedCheck: false,
+};
 let operator;
 
-const operation = {};
+const display = document.querySelector('#display');
+const numberButtons = document.querySelectorAll('.number-btn');
+const operatorButtons = document.querySelectorAll('.operator-btn');
+
+
+function populateDisplay(digit) {
+    display.textContent += digit;
+}
+
+function storeDisplay() {
+    return display.textContent;
+}
+
+function clearDisplay() {
+    display.textContent = "";
+}
+
+
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => populateDisplay(button.value));
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        
+        if (!firstNumber.storedCheck) {
+            firstNumber.number = storeDisplay();
+            firstNumber.storedCheck ^= true;
+            operator = button.value;
+            clearDisplay();
+        }
+
+        else if (!secondNumber.storedCheck) {
+            secondNumber.number = storeDisplay();
+            secondNumber.storedCheck ^= true;
+            clearDisplay();
+        }
+
+        else if (firstNumber.storedCheck && secondNumber.storedCheck) {
+            display.textContent = operate(operator, firstNumber.number, secondNumber.number);
+        }
+
+        
+    })
+});
+
+
+
+
+
